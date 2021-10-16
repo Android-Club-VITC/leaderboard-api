f59993859968e087d76eedd2315bcac60554cc88
Naman <naman.agarwal2019@vitstudent.ac.in>
2021-10-16 21:47:15 +0530

    add org header check to role verification middleware

--- middleware/role.js
+++ middleware/role.js
@@ -7,27 +7,34 @@ const Members = require("../models/members");
 const memberRolePipeline = require("../aggregationPipeline/memberRolePipeline");
 
 const verifyRole = (role) => {
-  return (req, res, next) => {
-    const d = decodeToken(req.header("ac_token"));
-    const h = req.header('X-AC-SUPER-ADMIN');
-    const o = req.header('orgId');
-    if (h == process.env.X_AC_SUPER_ADMIN){
-        req.superadmin = true
-        req.email = "*"
+  return async (req, res, next) => {
+    try {
+      const d = decodeToken(req.header("ac_token"));
+      const h = req.header("X-AC-SUPER-ADMIN");
+      const o = req.header("ac_orgId");
+      if (h == process.env.X_AC_SUPER_ADMIN) {
+        req.superadmin = true;
+        req.email = "*";
+        req.orgId = o
         next();
-    } else if(o) {
-      const r = Members.aggregate(memberRolePipeline.getUserRoleInOrg(email,o))
-
-      if (r.org?.role <= role) {
-        req.email = d.email  
-        req.orgId = orgId
-        next();
+      } else if (o) {
+        let r = await Members.aggregate(
+          memberRolePipeline.getUserRoleInOrg(d.email, o)
+        );
+        r = r[0];
+        if (r?.org.role <= role) {
+          req.email = d.email;
+          req.orgId = o;
+          next();
+        } else {
+          res.status(401).send();
+        }
       } else {
         res.status(401).send();
       }
-    }
-    else {  
-      res.status(401).send();  
+    } catch (e) {
+      console.log(e)
+      res.status(400).send();
     }
   };
 };
