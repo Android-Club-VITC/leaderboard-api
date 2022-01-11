09a7a021c37941be76dc705581e2a74118024394
Naman Agarwal <namanddev7@gmail.com>
2022-01-11 20:41:14 +0530

    fix otp

--- utils/otp.js
+++ utils/otp.js
@@ -1,6 +1,6 @@
 function generateOtp() {
     const digit = () => Math.floor(Math.random() * (9))
-    return `${digit()}${digit()}${digit()}${digit()}`;
+    return `${digit() % 8 + 1}${digit()}${digit()}${digit()}`;
 }
 
 module.exports = generateOtp;
